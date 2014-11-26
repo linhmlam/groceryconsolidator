171617dccaf57727844deaf852d134caf456f893
linhmlam <linh@chicagobooth.edu>
2014-11-25 21:14:59 -0600

    Can not db:migrate Recipe table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Recipe resource:
+  # CREATE
+  get('/recipes/new', { :controller => 'recipes', :action => 'new' })
+  get('/create_recipe', { :controller => 'recipes', :action => 'create' })
+
+  # READ
+  get('/recipes', { :controller => 'recipes', :action => 'index' })
+  get('/recipes/:id', { :controller => 'recipes', :action => 'show' })
+
+  # UPDATE
+  get('/recipes/:id/edit', { :controller => 'recipes', :action => 'edit' })
+  get('/update_recipe/:id', { :controller => 'recipes', :action => 'update' })
+
+  # DELETE
+  get('/delete_recipe/:id', { :controller => 'recipes', :action => 'destroy' })
+  #------------------------------
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
